d983fe1909bb998b95c8c49a30f77c7580e65cff
Hussen.Najjar <140242426+HussenNajjar@users.noreply.github.com>
2023-12-16 21:22:24 +0300

    Add files via upload

---
+++ s1.js
@@ -0,0 +1,14 @@
+ScrollReveal({ 
+    reset: true ,
+    distance:'60px',
+    duration:2500,
+    delay:150
+
+});
+
+ScrollReveal().reveal('.imgst1,.imgst2,.imgst3', { delay: 150 ,origin:'bottom' });
+ScrollReveal().reveal('.btn11,.btn22,.btn33', { delay: 150 ,origin:'top' });
+ScrollReveal().reveal('.fotimg', { delay: 200 ,origin:'right' });
+ScrollReveal().reveal('.fottext', { delay: 600 ,origin:'right' });
+ScrollReveal().reveal('.fottext1', { delay: 400 ,origin:'top' });
+
